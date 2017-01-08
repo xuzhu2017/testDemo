cd5148af5e55ab3799a9e2cacf69df5b22b9ecb4
xuzhu2017 <a753090302@163.com>
2017-01-08 20:57:34 +0800

    add

--- FirstFramework/Controllers/HomeController.cs
+++ FirstFramework/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace FirstFramework.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact.";
 
             return View();
         }
